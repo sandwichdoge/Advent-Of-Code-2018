92aa86c3692c1bd6b23f6e61a689d07bad152661
sandwichdoge <sandwichdoge@gmail.com>
2018-12-03 20:00:50 +0700

    find max fabric size for day3

--- day3/day3.py
+++ day3/day3.py
@@ -13,24 +13,36 @@ for line in lines:
         pcoords = p.findall(line)
         coords_list.append(pcoords[0])
 
-fabric = [[0 for i in range(1024)] for j in range(1024)]
-
-
-#Part1
-overlap_count = 0
+#Find max fabric size
+fh = 1024
+fw = 1024
 for coords in coords_list:
         left = int(coords[0])
         top = int(coords[1])
         width = int(coords[2])
         height = int(coords[3])
-        
+        if (left + width > fw): fw = left + width
+        if (top + height > fh): fh= top + height
+
+fabric = [[0 for i in range(fh)] for j in range(fw)]
+
+
+#Part1
+count = 0
+for coords in coords_list:
+        left = int(coords[0])
+        top = int(coords[1])
+        width = int(coords[2])
+        height = int(coords[3])
+
         for y in range(top, top + height):
                 for x in range(left, left + width):
                         if (fabric[y][x] == 1):
-                                overlap_count+= 1
+                                count+= 1
                         fabric[y][x] += 1
 
-print(overlap_count)
+print(count)
+
 
 #Part 2
 order = 0
@@ -41,13 +53,13 @@ for coords in coords_list: #I'm checking it twice
         height = int(coords[3])
         
         order += 1
-        oflag = 0
+        conflict = 0
 
         for y in range(top, top + height):
                 for x in range(left, left + width):
                         if (fabric[y][x] > 1): #I'm gonna find out who's naughty
-                                oflag = 1
+                                conflict = 1
                                 break
-        if (oflag == 0): #or nice
+        if (conflict == 0): #or nice
                 print(order)
                 break
