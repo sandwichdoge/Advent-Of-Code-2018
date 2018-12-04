62dad897c5a769e65709087d039f89fec604cbd5
sandwichdoge <sandwichdoge@gmail.com>
2018-12-04 23:02:39 +0700

    optimize day4 solution

--- day4/day4.py
+++ day4/day4.py
@@ -18,69 +18,13 @@ lines.sort()
 fd.close()
 
 
+#Parse and make sense of input
 guard = 0
 is_asleep = 0
 slept_since = 0
 sleep_duration = 0
 max_sleep = sleep_duration
 
-for line in lines:
-        p = re.compile(r"(\d+):(\d+)")
-        time = p.findall(line)[0]
-        p = re.compile(r"] (.+)")
-        action = p.findall(line)[0]
-        p = re.compile(r"#(\d+)")
-        guardRE = p.findall(action)
-        if (guardRE): 
-                guard = guardRE[0]
-                action = action[action.find("begins shift"):]
-
-        if (action == "begins shift"):
-                is_asleep = 0
-        elif (action == "falls asleep"):
-                is_asleep = 1
-                slept_since = time
-        elif (action == "wakes up" and is_asleep == 1):
-                sleep_duration = time_diff(slept_since, time)
-                if (max_sleep <= sleep_duration): 
-                        max_sleep = sleep_duration
-                        sleepyhead = guard
-
-
-#What minute does sleepyhead spend sleeping most?
-sleep_schedule = []
-for line in lines:
-        p = re.compile(r"(\d+):(\d+)")
-        time = p.findall(line)[0]
-        p = re.compile(r"] (.+)")
-        action = p.findall(line)[0]
-        p = re.compile(r"#(\d+)")
-        guardRE = p.findall(action)
-        if (guardRE): 
-                guard = guardRE[0]
-                action = action[action.find("begins shift"):]
-
-        if (guard == sleepyhead and action == "falls asleep"):
-                sleep_schedule.append(int(time[1]))
-        elif (guard == sleepyhead and action == "wakes up"):
-                sleep_schedule.append(int(time[1]))
-
-
-a = [0 for i in range(0, 60)]
-for i in range(0, len(sleep_schedule), 2):
-        for e in range(sleep_schedule[i], sleep_schedule[i+1]):
-                a[e] += 1
-
-m = 0
-chosen_minute = None
-for n in range(len(a)):
-        if (a[n] > m): 
-                m = a[n]
-                chosen_minute = n
-
-print(chosen_minute * int(sleepyhead))
-
-#Part 2
 sleep_schedule = []
 for line in lines:
         p = re.compile(r"(\d+):(\d+)")
@@ -100,15 +44,25 @@ for line in lines:
                 sleep_schedule.append(int(time[1]))
 
 
-a = [[0 for i in range(0, 60)] for j in range(4000)]#a[2000][60]
+a = [[0 for i in range(0, 60)] for j in range(4000)]#a[4000][60]
+total = [0] * 4000
 for i in range(0, len(sleep_schedule), 3):
         guard = sleep_schedule[i]
         sleepm = sleep_schedule[i+1]
         wakem = sleep_schedule[i+2]
         for e in range(sleepm, wakem):
                 a[guard][e] += 1
+                total[guard] += 1
 
 
+#Part 1
+sleepyhead = int(total.index(max(total)))
+chosen_minute = a[sleepyhead].index(max(a[sleepyhead]))
+
+print(chosen_minute * sleepyhead)
+
+
+#Part 2
 MAX = 0
 for guard in range(len(a)):
         for i in range (60):
@@ -117,4 +71,4 @@ for guard in range(len(a)):
                         final = guard
                         final_minute = i
 
-print(final, final_minute)
+print(final * final_minute)
